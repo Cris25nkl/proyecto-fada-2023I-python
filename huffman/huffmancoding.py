dc7649c343aa1ba3afc320d5e5b57d4bf6dc2cdf
JorgeEstacio <jorge.estacio@correounivalle.edu.co>
2023-07-11 14:19:07 -0500

    arreglo de indentación

--- huffman/huffmancoding.py
+++ huffman/huffmancoding.py
@@ -13,74 +13,73 @@ class HuffmanCoding:
     
     # Funcion para crear un arbol de huffman
 #  -------------------------------------------------------------------------- #
-  def crear_arbol(self, cadena):
+def crear_arbol(self, cadena):
     frecuencias = {}
     for caracter in cadena:
-      if caracter in frecuencias:
-        frecuencias[caracter] += 1
-      else:
-        frecuencias[caracter] = 1
+        if caracter in frecuencias:
+            frecuencias[caracter] += 1
+        else:
+            frecuencias[caracter] = 1
     
     heap=[]
     
     for caracter, frecuencia in frecuencias.items():
-      node = HuffmanBinaryTree(caracter, frecuencia)
-      heappush(heap, (frecuencia, node))
-      
-    while len(heap) > 1:
-      frecuencia1, node1 = heappop(heap)
-      frecuencia2, node2 = heappop(heap)
-      nodo_combinado = HuffmanBinaryTree(None, frecuencia1 + frecuencia2)
-      nodo_combinado.left = node1
-      nodo_combinado.right = node2
-      heappush(heap, (frecuencia1 + frecuencia2, nodo_combinado))
-      
+        node = HuffmanBinaryTree(caracter, frecuencia)
+        heappush(heap, (frecuencia, node))
+
+        while len(heap) > 1:
+            frecuencia1, node1 = heappop(heap)
+            frecuencia2, node2 = heappop(heap)
+            nodo_combinado = HuffmanBinaryTree(None, frecuencia1 + frecuencia2)
+            nodo_combinado.left = node1
+            nodo_combinado.right = node2
+            heappush(heap, (frecuencia1 + frecuencia2, nodo_combinado))
     return heappop(heap)[1]
 
 #  -------------------------------------------------------------------------- #
 
 from heapq import heappush, heappop, heapify
     
-    def getDiccionario(txt):
+def getDiccionario(txt):
     
-        frecuencia_letras = {}
+    frecuencia_letras = {}
 
-        for letra in txt:
-            if letra.isalnum() or letra in ['.',','] or letra.isspace():  # Verificar si es una letra o espacio
-                if letra in frecuencia_letras:
-                    frecuencia_letras[letra] += 1
-                else:
-                    frecuencia_letras[letra] = 1
-            
-        return frecuencia_letras
-
-    def encode(self, text):
-        """
-        Codifica el texto.
-        :param text: texto a codificar
-        :return: texto codificado
-        """
-        raise NotImplementedError("Aún no implementado")
-
-    def getTree(self):
-        """
-        Retorna el árbol de Huffman.
-        :return: árbol de Huffman
-        """
+    for letra in txt:
+        if letra.isalnum() or letra in ['.',','] or letra.isspace():  # Verificar si es una letra o espacio
+            if letra in frecuencia_letras:
+                frecuencia_letras[letra] += 1
+            else:
+                frecuencia_letras[letra] = 1
         
-        
-        raise NotImplementedError("Aún no implementado")
+    return frecuencia_letras
 
-    def getTable(self):
-        """
-        Retorna la tabla de codificación.
-        :return: tabla de codificación
-        """
-        raise NotImplementedError("Aún no implementado")
+def encode(self, text):
+    """
+    Codifica el texto.
+    :param text: texto a codificar
+    :return: texto codificado
+    """
+    raise NotImplementedError("Aún no implementado")
 
-    def getSummary(self):
-        """
-        Retorna el resumen de la codificación.
-        :return: resumen de la codificación en formato string
-        """
-        raise NotImplementedError("Aún no implementado")
+def getTree(self):
+    """
+    Retorna el árbol de Huffman.
+    :return: árbol de Huffman
+    """
+    
+    
+    raise NotImplementedError("Aún no implementado")
+
+def getTable(self):
+    """
+    Retorna la tabla de codificación.
+    :return: tabla de codificación
+    """
+    raise NotImplementedError("Aún no implementado")
+
+def getSummary(self):
+    """
+    Retorna el resumen de la codificación.
+    :return: resumen de la codificación en formato string
+    """
+    raise NotImplementedError("Aún no implementado")
